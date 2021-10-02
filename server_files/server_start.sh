ba1dcdbe2989e2fa27d738377688c809f3996717
CrushedKingoros <77001498+CrushedKingoros@users.noreply.github.com>
2021-10-02 23:05:40 +0900

    Update server_start.sh

--- server_files/server_start.sh
+++ server_files/server_start.sh
@@ -1,8 +1,1 @@
-@echo off
-cls
-echo This script will keep your server running even after crashing!
-title Minecraft Crashedetector
-:StartServer
-start /wait java -Xmx27G -jar purpur-1.17.1-1397.jar -o true
-echo (%time%) Server closed/crashed... restarting!
-goto StartServer
+java -Xmx27G -jar purpur-1.17.1-1397.jar nogui
